865da92bbf1f1b767abc1f4144420e2c265e0a91
gobomb <bombcoder@outlook.com>
2021-03-26 23:05:39 +0800

    add removeElement

---
+++ pkg/leetcode/remove_element.go
@@ -0,0 +1,35 @@
+package leetcode
+
+import "fmt"
+
+func removeElement(nums []int, val int) int {
+	j := 0
+	l := len(nums)
+
+	for i := 0; i < l; i++ {
+		if nums[i] == val {
+			continue
+		}
+		nums[j] = nums[i]
+		j++
+
+	}
+	return j
+}
+
+func testremoveElement() {
+	tests := [][]int{
+		{1},
+		{1, 1, 1, 3, 4, 4},
+		{1, 1, 2},
+		{0, 0, 1, 1, 1, 2, 2, 3, 3, 4},
+		{3, 2, 2, 3},
+		{0, 1, 2, 2, 3, 0, 4, 2},
+	}
+	for i := range tests {
+		r := removeElement(tests[i], 3)
+		fmt.Printf("%+v\n", tests[i])
+		fmt.Printf("result %v\n", r)
+		fmt.Printf("%+v\n\n", tests[i][:r])
+	}
+}
