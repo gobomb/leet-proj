e1aae357da21b7023f7fb3ef19e2c842e7ebf9d7
gobomb <bombcoder@outlook.com>
2021-04-07 00:23:06 +0800

    update 3sum-closest

--- pkg/leetcode/3sum-closest.go
+++ pkg/leetcode/3sum-closest.go
@@ -23,6 +23,35 @@ func threeSumClosest(nums []int, target int) int {
 	return result
 }
 
+func threeSumClosestDoublePointer(nums []int, target int) int {
+	sort.Ints(nums)
+	// fmt.Printf("%+v\n", nums)
+	res, sub := 0, 1000
+	for i := 0; i < len(nums)-2; i++ {
+		for j, k := i+1, len(nums)-1; k > j; {
+			// for k := j + 1; k < len(nums); k++ {
+			sum := nums[i] + nums[j] + nums[k]
+			if sub > abs(sum-target) {
+				sub = abs(sum - target)
+				res = sum
+			}
+			//println(i, j, k, nums[i], nums[j], nums[k], sum, res)
+
+			if sum == target {
+				return sum
+			}
+			if sum > target {
+				k--
+			}
+			if sum < target {
+				j++
+			}
+		}
+	}
+
+	return res
+}
+
 func abs(x int) int {
 	if x < 0 {
 		return -x
--- pkg/leetcode/3sum-closest_test.go
+++ pkg/leetcode/3sum-closest_test.go
@@ -1,22 +1,27 @@
 package leetcode
 
-import "testing"
+import (
+	"testing"
+)
+
+type args struct {
+	nums   []int
+	target int
+}
+
+var tests = []struct {
+	name string
+	args args
+	want int
+}{
+	{"1", args{nums: []int{-1, 2, 1, -4}, target: 1}, 2},
+	{"2", args{nums: []int{-1, 2, 1, 4}, target: 1}, 2},
+	{"3", args{nums: []int{2, 2, 2, 2}, target: 1}, 6},
+	{"4", args{nums: []int{1, 1, 1, 0}, target: 100}, 3},
+	// TODO: Add test cases.
+}
 
 func Test_threeSumClosest(t *testing.T) {
-	type args struct {
-		nums   []int
-		target int
-	}
-	tests := []struct {
-		name string
-		args args
-		want int
-	}{
-		{"1", args{nums: []int{-1, 2, 1, -4}, target: 1}, 2},
-		{"2", args{nums: []int{-1, 2, 1, 4}, target: 1}, 2},
-		{"3", args{nums: []int{2, 2, 2, 2}, target: 1}, 6},
-		// TODO: Add test cases.
-	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := threeSumClosest(tt.args.nums, tt.args.target); got != tt.want {
@@ -25,3 +30,35 @@ func Test_threeSumClosest(t *testing.T) {
 		})
 	}
 }
+
+func Test_threeSumClosestDoublePointer(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := threeSumClosestDoublePointer(tt.args.nums, tt.args.target); got != tt.want {
+				t.Errorf("threeSumClosestDoublePointer() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+// func Benchmark_threeSumClosestDoublePointer(b *testing.B) {
+// 	for i := 0; i < b.N; i++ {
+// 		b.ReportAllocs()
+// 		for _, tt := range tests {
+// 			if got := threeSumClosestDoublePointer(tt.args.nums, tt.args.target); got != tt.want {
+// 			}
+// 			break
+// 		}
+// 	}
+// }
+
+// func Benchmark_threeSumClosest(b *testing.B) {
+// 	for i := 0; i < b.N; i++ {
+// 		b.ReportAllocs()
+// 		for _, tt := range tests {
+// 			if got := threeSumClosest(tt.args.nums, tt.args.target); got != tt.want {
+// 			}
+// 			break
+// 		}
+// 	}
+// }
