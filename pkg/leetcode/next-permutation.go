2f1695014d27af3d01a5fbc7591e357e65ed1cf3
gobomb <bombcoder@outlook.com>
2021-04-18 22:15:30 +0800

    add next-permutation

---
+++ pkg/leetcode/next-permutation.go
@@ -0,0 +1,46 @@
+package leetcode
+
+/*
+1. 从尾部找到第一个不符合升序的数字a
+2. 从尾部找到第一个比a大的数字b
+3. 交换 a b 位置
+4. 将 b（交换后的b位置）后的序列翻转
+*/
+
+func nextPermutation(nums []int) {
+	var a, b, i int
+	for i = len(nums) - 1; i > 0; i-- {
+		if nums[i-1] < nums[i] {
+			a = i - 1
+			break
+		}
+	}
+	// 从尾部算全部都是降序，已经是最大了。直接逆序返回
+	if i == 0 {
+		reverseInt(nums[:])
+		return
+	}
+
+	for i := len(nums) - 1; i > a; i-- {
+		if nums[a] < nums[i] {
+			b = i
+			break
+		}
+	}
+
+	swap(nums, a, b)
+
+	reverseInt(nums[a+1:])
+
+	return
+}
+
+func reverseInt(nums []int) {
+	for i, j := 0, len(nums)-1; i < j; i, j = i+1, j-1 {
+		swap(nums, i, j)
+	}
+}
+
+func swap(nums []int, a, b int) {
+	nums[a], nums[b] = nums[b], nums[a]
+}
---
+++ pkg/leetcode/next-permutation_test.go
@@ -0,0 +1,77 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_nextPermutation(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+		{
+			"1",
+			args{
+				[]int{1, 2, 3},
+			},
+			[]int{1, 3, 2},
+		},
+		{
+			"2",
+			args{
+				[]int{3, 2, 1},
+			},
+			[]int{1, 2, 3},
+		},
+		{
+			"3",
+			args{
+				[]int{1, 1, 5},
+			},
+			[]int{1, 5, 1},
+		},
+		{
+			"4",
+			args{
+				[]int{1},
+			},
+			[]int{1},
+		},
+		{
+			"5",
+			args{
+				[]int{8, 9, 6, 10, 7, 2},
+			},
+			[]int{8, 9, 7, 2, 6, 10},
+		},
+		{
+			"6",
+			args{
+				[]int{1, 2, 7, 4, 3, 1},
+			},
+			[]int{1, 3, 1, 2, 4, 7},
+		},
+		{
+			"4",
+			args{
+				[]int{2, 3, 1},
+			},
+			[]int{3, 1, 2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// arg := make([]int, len(tt.args.nums))
+			// copy(arg, tt.args.nums)
+			if nextPermutation(tt.args.nums); !reflect.DeepEqual(tt.args.nums, tt.want) {
+				t.Errorf("isMatch() = %v, want %v", tt.args.nums, tt.want)
+			}
+		})
+	}
+}
