77d49f0450fd1c106fec25a996cd094a869d3c31
gobomb <bombcoder@outlook.com>
2021-04-24 10:21:37 +0800

    add quick sort

--- pkg/ds/qsort.go
+++ pkg/ds/qsort.go
@@ -1,5 +1,44 @@
 package ds
 
-func quick(arr []int) {
-	return
+func quick(arr []int) []int {
+	QuickSort(arr, 0, len(arr)-1)
+	return arr
+}
+func QuickSort(arr []int, startIndex, endIndex int) {
+	if startIndex >= endIndex {
+		return
+	}
+
+	pivotIndex := partition(arr, startIndex, endIndex)
+	QuickSort(arr, startIndex, pivotIndex-1)
+	QuickSort(arr, pivotIndex+1, endIndex)
+}
+
+// https://segmentfault.com/a/1190000020229121
+func partition(arr []int, startIndex, endIndex int) int {
+	var (
+		i   = startIndex
+		j   = endIndex
+		piv = arr[startIndex]
+	)
+
+	for i != j {
+		// <= ;顺序
+		for i < j && arr[j] > piv {
+			j--
+		}
+		for i < j && arr[i] <= piv {
+			i++
+		}
+
+		if i < j {
+			swap(arr, i, j)
+		}
+	}
+	swap(arr, startIndex, i)
+	return i
+}
+
+func swap(nums []int, a, b int) {
+	nums[a], nums[b] = nums[b], nums[a]
 }
--- pkg/ds/qsort_test.go
+++ pkg/ds/qsort_test.go
@@ -1,16 +1,22 @@
 package ds
 
 import (
+	"reflect"
 	"sort"
 	"testing"
 )
 
 func genRandSlice(length, max int) ([]int, []int) {
 	r := randSliceInt(max, length)
-	sr := make([]int, length)
-	copy(sr, r)
-	sort.Ints(sr)
-	return r, sr
+	rcopy := deepCopyIntSlice(r)
+	sort.Ints(rcopy)
+	return r, rcopy
+}
+
+func deepCopyIntSlice(arr []int) []int {
+	rcopy := make([]int, len(arr))
+	copy(rcopy, arr)
+	return rcopy
 }
 
 func Test_quick(t *testing.T) {
@@ -25,8 +31,8 @@ func Test_quick(t *testing.T) {
 		// TODO: Add test cases.
 	}
 
-	for i := 0; i < 5; i++ {
-		r, sr := genRandSlice(15, 50)
+	for i := 0; i < 3; i++ {
+		r, sr := genRandSlice(3, 50)
 		tests = append(tests, struct {
 			name string
 			args args
@@ -34,10 +40,30 @@ func Test_quick(t *testing.T) {
 		}{"", args{r}, sr})
 		t.Logf("%v\n", tests[i])
 	}
+	arr := []int{15, 25, 25, 18, 17, 48, 12, 25, 2, 8, 15, 20, 27, 46, 48}
+	sarr := deepCopyIntSlice(arr)
+	sort.Ints(sarr)
+	tests = append(tests, struct {
+		name string
+		args args
+		want []int
+	}{"", args{arr}, sarr})
+
+	for i := 0; i < 15; i++ {
+		r, sr := genRandSlice(15, 50)
+		tests = append(tests, struct {
+			name string
+			args args
+			want []int
+		}{"", args{r}, sr})
+		// t.Logf("%v\n", tests[i])
+	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			quick(tt.args.arr)
+			if got := quick(deepCopyIntSlice(tt.args.arr)); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("quick(%v) = %v, want %v", tt.args.arr, got, tt.want)
+			}
 		})
 	}
 }
