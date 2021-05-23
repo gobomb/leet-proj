380df19f7351e7ce86b69c3020676002885e7b6b
gobomb <bombcoder@outlook.com>
2021-05-23 18:47:03 +0800

    update valid-number

--- pkg/leetcode/valid-number.go
+++ pkg/leetcode/valid-number.go
@@ -1,8 +1,51 @@
 package leetcode
 
-import "log"
+const (
+	INVALID int = iota
+	SPACE
+	SIGN
+	DIGIT
+	DOT
+	EXPONENT
+)
+
+// ref: https://zhuanlan.zhihu.com/p/20042325`
+var transitionTable = [][]int{{-1, 0, 3, 1, 2, -1}, //0 no input or just spaces
+	{-1, 8, -1, 1, 4, 5},    //1 input is digits
+	{-1, -1, -1, 4, -1, -1}, //2 no digits in front just Dot
+	{-1, -1, -1, 1, 2, -1},  //3 sign
+	{-1, 8, -1, 4, -1, 5},   //4 digits and dot in front
+	{-1, -1, 6, 7, -1, -1},  //5 input 'e' or 'E'
+	{-1, -1, -1, 7, -1, -1}, //6 after 'e' input sign
+	{-1, 8, -1, 7, -1, -1},  //7 after 'e' input digits
+	{-1, 8, -1, -1, -1, -1}} //8 after valid input input space
 
 func isNumber(s string) bool {
-	log.Printf("%v\n", "sdsd")
-	return true
+	state := INVALID
+
+	for i := range s {
+		c := s[i]
+		tp := INVALID
+		switch c {
+		case ' ':
+			tp = SPACE
+		case '+', '-':
+			tp = SIGN
+		case '1', '2', '3', '4', '5', '6', '7', '8', '9', '0':
+			tp = DIGIT
+		case '.':
+			tp = DOT
+		case 'e', 'E':
+			tp = EXPONENT
+		}
+		state = transitionTable[state][tp]
+		if state == -1 {
+			return false
+		}
+	}
+	switch state {
+	case 1, 4, 7, 8:
+		return true
+	}
+	return false
 }
--- pkg/leetcode/valid-number_test.go
+++ pkg/leetcode/valid-number_test.go
@@ -11,7 +11,6 @@ func init() {
 
 func Test_isNumber(t *testing.T) {
 	trueTests := []string{"2", "0089", "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93", "-123.456e789"}
-	falseTests := []string{"abc", "1a", "1e", "e3", "99e2.5", "--6", "-+3", "95a54e53"}
 	for _, v := range trueTests {
 		t.Run("true tests", func(t *testing.T) {
 			if got := isNumber(v); got != true {
@@ -19,6 +18,7 @@ func Test_isNumber(t *testing.T) {
 			}
 		})
 	}
+	falseTests := []string{"abc", "1a", "1e", "e3", "99e2.5", "--6", "-+3", "95a54e53"}
 	for _, v := range falseTests {
 		t.Run("false tests", func(t *testing.T) {
 			if got := isNumber(v); got != false {
