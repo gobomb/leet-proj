f8805817693d6d6fd3cfa6620e0ea05a6d8e67da
gobomb <bombcoder@outlook.com>
2021-05-30 17:56:10 +0800

    add leetcode 82

--- pkg/leetcode/remove-duplicates-from-sorted-list.go
+++ pkg/leetcode/remove-duplicates-from-sorted-list.go
@@ -1,5 +1,7 @@
 package leetcode
 
+import "fmt"
+
 func deleteDuplicates(head *ListNode) *ListNode {
 	h := head
 	l := &ListNode{Val: -101}
@@ -14,4 +16,89 @@ func deleteDuplicates(head *ListNode) *ListNode {
 		h = h.Next
 	}
 	return head
+}
+
+func deleteDuplicates2(head *ListNode) *ListNode {
+	h := head
+	for h.Next != nil {
+		if h.Val == h.Next.Val {
+			h.Next = h.Next.Next
+			continue
+		}
+		h = h.Next
+	}
+	return head
+}
+
+func deleteDuplicatesII(head *ListNode) *ListNode {
+	h := head
+	l := &ListNode{Val: -101}
+	saved := make(map[int]struct{})
+	for h != nil {
+		if l.Val == h.Val {
+			l.Next = h.Next
+			h = h.Next
+			saved[l.Val] = struct{}{}
+			continue
+		}
+		l = h
+		h = h.Next
+	}
+	h = head
+	for h != nil {
+		if _, ok := saved[h.Val]; ok {
+			if h == head {
+				head = h.Next
+				l = h
+				h = h.Next
+				continue
+			}
+			l.Next = h.Next
+			h = h.Next
+		} else {
+			l = h
+			h = h.Next
+		}
+	}
+	return head
+}
+
+// https://books.halfrost.com/leetcode/ChapterFour/0001~0099/0082.Remove-Duplicates-from-Sorted-List-II/
+func deleteDuplicatesII2(head *ListNode) *ListNode {
+	newHead := &ListNode{Next: head, Val: -999999}
+	cur := newHead
+	last := newHead
+	front := head
+	for front.Next != nil {
+		if front.Val == cur.Val {
+			front = front.Next
+			continue
+		} else {
+			if cur.Next != front {
+				last.Next = front
+				if front.Next != nil && front.Next.Val != front.Val {
+					last = front
+				}
+				cur = front
+				front = front.Next
+				if front == nil {
+					fmt.Println("ss")
+				}
+			} else {
+				front = front.Next
+				last = cur
+				cur = cur.Next
+			}
+		}
+	}
+
+	if front.Val == cur.Val {
+		last.Next = nil
+	} else {
+		if cur.Next != front {
+			last.Next = front
+		}
+	}
+
+	return newHead.Next
 }
--- pkg/leetcode/remove-duplicates-from-sorted-list_test.go
+++ pkg/leetcode/remove-duplicates-from-sorted-list_test.go
@@ -42,3 +42,68 @@ func Test_deleteDuplicates(t *testing.T) {
 		})
 	}
 }
+
+func Test_deleteDuplicatesII(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			name: "1",
+			args: args{
+				head: makeListNode([]int{1, 1, 2}),
+			},
+			want: makeListNode([]int{2}),
+		},
+		{
+			name: "2",
+			args: args{
+				head: makeListNode([]int{1, 1, 2, 3, 3}),
+			},
+			want: makeListNode([]int{2}),
+		}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := deleteDuplicatesII(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("deleteDuplicatesII() = %+v, want %+v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_deleteDuplicatesII2(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{{
+		name: "1",
+		args: args{
+			head: makeListNode([]int{1, 1, 2}),
+		},
+		want: makeListNode([]int{2}),
+	},
+		{
+			name: "2",
+			args: args{
+				head: makeListNode([]int{1, 1, 2, 3, 3}),
+			},
+			want: makeListNode([]int{2}),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := deleteDuplicatesII2(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("deleteDuplicatesII2() = %+v, want %+v", got, tt.want)
+			}
+		})
+	}
+}
