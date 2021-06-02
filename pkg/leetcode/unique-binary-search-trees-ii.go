20224cdd45a080dc671cbd436e90a0742d0a9eba
gobomb <bombcoder@outlook.com>
2021-06-02 17:34:34 +0800

    add leetcode 96

--- pkg/leetcode/unique-binary-search-trees-ii.go
+++ pkg/leetcode/unique-binary-search-trees-ii.go
@@ -1,19 +1,18 @@
 package leetcode
 
 func generateTrees(n int) []*TreeNode {
-	return bfsGenTrees(1, n)
+	return genBSTrees(1, n)
 }
 
-func bfsGenTrees(s, e int) []*TreeNode {
+func genBSTrees(s, e int) []*TreeNode {
 	var rs []*TreeNode
 	if s > e {
 		rs = append(rs, nil)
 		return rs
 	}
 	for i := s; i <= e; i++ {
-
-		left := bfsGenTrees(s, i-1)
-		right := bfsGenTrees(i+1, e)
+		left := genBSTrees(s, i-1)
+		right := genBSTrees(i+1, e)
 		for _, l := range left {
 			for _, r := range right {
 				root := &TreeNode{
@@ -26,4 +25,19 @@ func bfsGenTrees(s, e int) []*TreeNode {
 		}
 	}
 	return rs
+}
+
+// Catalan number
+// https://zh.wikipedia.org/wiki/%E5%8D%A1%E5%A1%94%E5%85%B0%E6%95%B0
+func numTrees(n int) int {
+	dp := make([]int, n+1)
+	dp[0] = 1
+	dp[1] = 1
+	for i := 2; i <= n; i++ {
+		for j := 0; j < i; j++ {
+			dp[i] += dp[j] * dp[i-j-1]
+		}
+	}
+
+	return dp[n]
 }
--- pkg/leetcode/unique-binary-search-trees-ii_test.go
+++ pkg/leetcode/unique-binary-search-trees-ii_test.go
@@ -44,3 +44,43 @@ func Test_generateTrees(t *testing.T) {
 		})
 	}
 }
+
+func Test_numTrees(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "1",
+			args: args{
+				n: 3,
+			},
+			want: 5,
+		},
+		{
+			name: "2",
+			args: args{
+				n: 1,
+			},
+			want: 1,
+		},
+		{
+			name: "3",
+			args: args{
+				n: 2,
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numTrees(tt.args.n); got != tt.want {
+				t.Errorf("numTrees(%v) = %v, want %v", tt.args.n, got, tt.want)
+			}
+		})
+	}
+}
