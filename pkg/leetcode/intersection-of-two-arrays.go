80000a62f3d4804f8ff55464c980bb721c80826c
gobomb <bombcoder@outlook.com>
2021-06-19 19:07:37 +0800

    add leetcode 350

--- pkg/leetcode/intersection-of-two-arrays.go
+++ pkg/leetcode/intersection-of-two-arrays.go
@@ -15,3 +15,27 @@ func intersection(nums1 []int, nums2 []int) []int {
 	}
 	return rs
 }
+
+func intersect2(nums1 []int, nums2 []int) []int {
+	m := make(map[int]int)
+	rs := []int{}
+	for i := range nums1 {
+		_, ok := m[nums1[i]]
+		if ok {
+			m[nums1[i]] = m[nums1[i]] + 1
+			continue
+		}
+		m[nums1[i]] = 1
+	}
+	for i := range nums2 {
+		_, ok := m[nums2[i]]
+		if ok {
+			rs = append(rs, nums2[i])
+			m[nums2[i]] = m[nums2[i]] - 1
+			if m[nums2[i]] == 0 {
+				delete(m, nums2[i])
+			}
+		}
+	}
+	return rs
+}
