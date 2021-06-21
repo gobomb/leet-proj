007174a1aa0ecd1ebf7c26db1e92f4516f24a382
gobomb <bombcoder@outlook.com>
2021-06-21 18:54:53 +0800

    update leetcode 102

--- pkg/leetcode/binary-tree-level-order-traversal.go
+++ pkg/leetcode/binary-tree-level-order-traversal.go
@@ -2,13 +2,8 @@ package leetcode
 
 func levelOrder(root *TreeNode) [][]int {
 	rs := [][]int{}
-	rs = append(rs, []int{})
 	getLevelOrder(root, 0, &rs)
-	for i := len(rs) - 1; i >= 0; i-- {
-		if len(rs[i]) == 0 {
-			rs = rs[:len(rs)-1]
-		}
-	}
+
 	return rs
 }
 
@@ -16,10 +11,11 @@ func getLevelOrder(root *TreeNode, depth int, rs *[][]int) {
 	if root == nil {
 		return
 	}
+	if len(*rs) == depth {
+		*rs = append(*rs, []int{})
+	}
 	(*rs)[depth] = append((*rs)[depth], root.Val)
-	*rs = append(*rs, []int{})
 
 	getLevelOrder(root.Left, depth+1, rs)
 	getLevelOrder(root.Right, depth+1, rs)
-
 }
