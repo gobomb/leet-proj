f63d9992a987774ae66b7665b3e7ab76c8d10133
gobomb <bombcoder@outlook.com>
2021-06-25 17:01:09 +0800

    add leetcode 108

--- pkg/leetcode/convert-sorted-array-to-binary-search-tree.go
+++ pkg/leetcode/convert-sorted-array-to-binary-search-tree.go
@@ -4,11 +4,20 @@ func sortedArrayToBST(nums []int) *TreeNode {
 	if len(nums) == 0 {
 		return nil
 	}
-	val := nums[len(nums)/2]
+	mid := len(nums) / 2
 	tree := &TreeNode{
-		Val:   val,
-		Left:  sortedArrayToBST(nums[:len(nums)/2]),
-		Right: sortedArrayToBST(nums[len(nums)/2+1:]),
+		Val:   nums[mid],
+		Left:  sortedArrayToBST(nums[:mid]),
+		Right: sortedArrayToBST(nums[mid+1:]),
 	}
 	return tree
+}
+
+func sortedListToBST(head *ListNode) *TreeNode {
+	nums := []int{}
+	for head != nil {
+		nums = append(nums, head.Val)
+		head = head.Next
+	}
+	return sortedArrayToBST(nums)
 }
--- pkg/leetcode/convert-sorted-array-to-binary-search-tree_test.go
+++ pkg/leetcode/convert-sorted-array-to-binary-search-tree_test.go
@@ -1,9 +1,14 @@
 package leetcode
 
 import (
+	"log"
 	"reflect"
 	"testing"
 )
+
+func init() {
+	log.SetFlags(log.Lshortfile)
+}
 
 func Test_sortedArrayToBST(t *testing.T) {
 	type args struct {
@@ -33,6 +38,39 @@ func Test_sortedArrayToBST(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := sortedArrayToBST(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("sortedArrayToBST() = %+v, want %+v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_sortedListToBST(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		{
+			name: "1",
+			args: args{
+				head: MakeListNode(-10, -3, 0, 5, 9),
+			},
+			want: MakeTree([]int{0, -3, 9, -10, Null, 5}),
+		},
+		{
+			name: "2",
+			args: args{
+				head: MakeListNode(1, 3),
+			},
+			want: MakeTree([]int{3, 1}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sortedListToBST(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sortedListToBST(%v) = %v, want %v", tt.args.head, got, tt.want)
 			}
 		})
 	}
