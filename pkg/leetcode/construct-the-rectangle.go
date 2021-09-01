0374738a36cb2450e886605059e23bb97ad2798f
gobomb <bombcoder@outlook.com>
2021-09-01 17:16:26 +0800

    add leetcode 492

---
+++ pkg/leetcode/construct-the-rectangle.go
@@ -0,0 +1,26 @@
+package leetcode
+
+func constructRectangle(area int) []int {
+	var l, w, sub int
+	l = area
+	res := []int{0, 0}
+	for w = 1; w <= l; w++ {
+
+		if area%w != 0 {
+			continue
+		}
+
+		l = area / w
+		if l-w == 0 {
+			return []int{l, w}
+		}
+		if sub == 0 || l-w < sub {
+
+			sub = l - w
+			// log.Println(l, w, sub)
+
+			res = []int{w, l}
+		}
+	}
+	return res
+}
---
+++ pkg/leetcode/construct-the-rectangle_test.go
@@ -0,0 +1,46 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_constructRectangle(t *testing.T) {
+	type args struct {
+		area int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "1",
+			args: args{
+				area: 4,
+			},
+			want: []int{2, 2},
+		},
+		{
+			name: "2",
+			args: args{
+				area: 37,
+			},
+			want: []int{37, 1},
+		},
+		{
+			name: "3",
+			args: args{
+				area: 122122,
+			},
+			want: []int{427, 286},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := constructRectangle(tt.args.area); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("constructRectangle() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
