3571bd0e88be7f2795069fc6ff8e28a721f6ff5c
gobomb <bombcoder@outlook.com>
2021-10-22 15:13:28 +0000

    add 120. Triangle

--- pkg/leetcode/triangle.go
+++ pkg/leetcode/triangle.go
@@ -1,16 +1,36 @@
 package leetcode
 
-func minimumTotal(triangle [][]int) int {
-	rs := 0
-	j := 0
+/*
+	120. Triangle
+*/
 
-	for i := range triangle {
-		if i == 0 {
-			rs += triangle[i][j]
-		} else if triangle[i][j] < triangle[i][j+1] {
-			rs += triangle[i][j]
-		} else {
-			rs += triangle[i][j+1]
+func minimumTotal(triangle [][]int) int {
+	if len(triangle) == 0 {
+		return 0
+	}
+	rs := 10001
+	dp := make([]int, len(triangle))
+
+	for i := 0; i < len(triangle[0]); i++ {
+		dp[i] = triangle[0][i]
+	}
+
+	for i := 1; i < len(triangle); i++ {
+		for j := len(triangle[i]) - 1; j >= 0; j-- {
+			if j == 0 {
+				dp[j] = dp[j] + triangle[i][j]
+			} else if j == len(triangle[i])-1 {
+				dp[j] = dp[j] + dp[j-1] + triangle[i][j]
+			} else {
+				dp[j] = min(dp[j-1]+triangle[i][j], dp[j]+triangle[i][j])
+			}
+		}
+	}
+
+	// log.Printf("%v", dp)
+	for i := 0; i < len(dp); i++ {
+		if dp[i] < rs {
+			rs = dp[i]
 		}
 	}
 	return rs
