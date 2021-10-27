96bc5eeeb207c2d8097e9083078feb4f3872b1df
gobomb <bombcoder@outlook.com>
2021-10-27 06:43:53 +0000

    update leetcode 127

--- pkg/leetcode/word-ladder.go
+++ pkg/leetcode/word-ladder.go
@@ -14,6 +14,10 @@ func isWordInList(s string, wordList []string) bool {
 	return found
 }
 
+/*
+Runtime: 152 ms, faster than 65.58% of Go online submissions for Word Ladder.
+Memory Usage: 7.5 MB, less than 51.37% of Go online submissions for Word Ladder.
+*/
 func ladderLength(beginWord string, endWord string, wordList []string) int {
 	if len(wordList) == 0 {
 		return 0
@@ -27,10 +31,6 @@ func ladderLength(beginWord string, endWord string, wordList []string) int {
 		return 1
 	}
 
-	if !isWordInList(beginWord, wordList) {
-		wordList = append(wordList, beginWord)
-	}
-
 	wordmap := make(map[string]struct{})
 	for _, w := range wordList {
 		wordmap[w] = struct{}{}
@@ -39,7 +39,6 @@ func ladderLength(beginWord string, endWord string, wordList []string) int {
 	queue := make([]string, 0)
 	visited := make(map[string]struct{})
 	queue = append(queue, beginWord)
-	visited[beginWord] = struct{}{}
 	rs := 1
 
 	for len(queue) != 0 {
@@ -80,4 +79,69 @@ func getAdjacents(w string, list map[string]struct{}) (adj []string) {
 		}
 	}
 	return adj
+}
+
+/*
+Runtime: 24 ms, faster than 99.43% of Go online submissions for Word Ladder.
+Memory Usage: 6.6 MB, less than 74.71% of Go online submissions for Word Ladder.
+*/
+func ladderLengthBiDBFS(beginWord string, endWord string, wordList []string) int {
+	if len(wordList) == 0 {
+		return 0
+	}
+
+	if !isWordInList(endWord, wordList) {
+		return 0
+	}
+
+	if beginWord == endWord {
+		return 1
+	}
+
+	wordmap := make(map[string]struct{})
+	for _, w := range wordList {
+		wordmap[w] = struct{}{}
+	}
+
+	beginQueue := []string{beginWord}
+	endQueue := []string{endWord}
+	visited := make(map[string]struct{})
+
+	rs := 1
+
+	for len(beginQueue) != 0 && len(endQueue) != 0 {
+		if len(beginQueue) > len(endQueue) {
+			beginQueue, endQueue = endQueue, beginQueue
+		}
+
+		rs++
+
+		tmpQueue := []string{}
+
+		for _, w := range beginQueue {
+			// log.Println(w)
+			adj := getAdjacents(w, wordmap)
+			for _, a := range adj {
+				if contain(endQueue, a) {
+					return rs
+				}
+				if _, ok := visited[a]; !ok {
+					tmpQueue = append(tmpQueue, a)
+					visited[a] = struct{}{}
+				}
+			}
+		}
+		beginQueue = tmpQueue
+	}
+
+	return 0
+}
+
+func contain(list []string, s string) bool {
+	for _, a := range list {
+		if a == s {
+			return true
+		}
+	}
+	return false
 }
--- pkg/leetcode/word-ladder_test.go
+++ pkg/leetcode/word-ladder_test.go
@@ -51,3 +51,60 @@ func Test_ladderLength(t *testing.T) {
 		})
 	}
 }
+
+func Test_ladderLengthBiDBFS(t *testing.T) {
+	type args struct {
+		beginWord string
+		endWord   string
+		wordList  []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "1",
+			args: args{
+				beginWord: "hit",
+				endWord:   "cog",
+				wordList:  []string{"hot", "dot", "dog", "lot", "log", "cog"},
+			},
+			want: 5,
+		},
+		{
+			name: "2",
+			args: args{
+				beginWord: "hit",
+				endWord:   "cog",
+				wordList:  []string{"hot", "dot", "dog", "lot", "log"},
+			},
+			want: 0,
+		},
+		{
+			name: "3",
+			args: args{
+				beginWord: "a",
+				endWord:   "c",
+				wordList:  []string{"a", "b", "c"},
+			},
+			want: 2,
+		},
+		{
+			name: "4",
+			args: args{
+				beginWord: "hot",
+				endWord:   "dog",
+				wordList:  []string{"hot", "cog", "dog", "tot", "hog", "hop", "pot", "dot"},
+			},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ladderLengthBiDBFS(tt.args.beginWord, tt.args.endWord, tt.args.wordList); got != tt.want {
+				t.Errorf("ladderLengthBiDBFS() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
