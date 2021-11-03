31195effc7d2061086b26d1292be7f5550e5eae2
gobomb <bombcoder@outlook.com>
2021-11-03 06:17:39 +0000

    add leetcode 134. Gas Station

---
+++ pkg/leetcode/medium/134.go
@@ -0,0 +1,22 @@
+package medium
+
+/*
+	134. Gas Station
+*/
+
+func canCompleteCircuit(gas []int, cost []int) int {
+	tank, sum, start := 0, 0, 0
+	for i := 0; i < len(gas); i++ {
+		tank += gas[i] - cost[i]
+		sum += gas[i] - cost[i]
+		if sum < 0 {
+			start = i + 1
+			sum = 0
+		}
+	}
+	if tank < 0 {
+		return -1
+	}
+
+	return start
+}
