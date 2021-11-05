f53a03b2bd727109cd795f40e6a63ecb5466a68e
gobomb <bombcoder@outlook.com>
2021-11-05 13:25:29 +0000

    add leetcode 167. Two Sum II - Input Array Is Sorted

---
+++ pkg/leetcode/easy/167.go
@@ -0,0 +1,39 @@
+package easy
+
+/*
+	167. Two Sum II - Input Array Is Sorted
+*/
+
+// 132 ms	3.2 MB
+func twoSum(numbers []int, target int) []int {
+	for i := 0; i < len(numbers); i++ {
+		for j := i + 1; j < len(numbers); j++ {
+			if numbers[i]+numbers[j] == target {
+				return []int{i + 1, j + 1}
+			}
+		}
+	}
+	return []int{}
+}
+
+// 	4 ms	3 MB
+func twoSumBiSe(numbers []int, target int) []int {
+	for i := range numbers {
+		target -= numbers[i]
+		lo, hi, mid := i+1, len(numbers)-1, 0
+		for lo <= hi {
+			mid = (lo + hi) / 2
+
+			switch {
+			case target > numbers[mid]:
+				lo = mid + 1
+			case target < numbers[mid]:
+				hi = mid - 1
+			case target == numbers[mid]:
+				return []int{i + 1, mid + 1}
+			}
+		}
+		target += numbers[i]
+	}
+	return []int{}
+}
---
+++ pkg/leetcode/easy/167_test.go
@@ -0,0 +1,85 @@
+package easy
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_twoSum(t *testing.T) {
+	type args struct {
+		numbers []int
+		target  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "1",
+			args: args{
+				numbers: []int{2, 7, 11, 15},
+				target:  9,
+			},
+			want: []int{1, 2},
+		},
+		{
+			name: "2",
+			args: args{
+				numbers: []int{2, 3, 4},
+				target:  6,
+			},
+			want: []int{1, 3},
+		},
+		{
+			name: "3",
+			args: args{
+				numbers: []int{-1, 0},
+				target:  -1,
+			},
+			want: []int{1, 2},
+		},
+		{
+			name: "4",
+			args: args{
+				numbers: []int{-1, -2, 0},
+				target:  -1,
+			},
+			want: []int{1, 3},
+		},
+		/*
+					[12,13,23,28,43,44,59,60,61,68,70,86,88,92,124,125,136,168,173,173,180,199,212,221,227,230,277,282,306,314,316,321,325,328,336,337,363,365,368,370,370,371,375,384,387,394,400,404,414,422,422,427,430,435,457,493,506,527,531,538,541,546,568,583,585,587,650,652,677,691,730,737,740,751,755,764,778,783,785,789,794,803,809,815,847,858,863,863,874,887,896,916,920,926,927,930,933,957,981,997]
+			542
+		*/
+		{
+			name: "5",
+			args: args{
+				numbers: []int{12, 13, 23, 28, 43, 44, 59, 60, 61, 68, 70, 86, 88, 92, 124, 125, 136, 168, 173, 173, 180, 199, 212, 221, 227, 230, 277, 282, 306, 314, 316, 321, 325, 328, 336, 337, 363, 365, 368, 370, 370, 371, 375, 384, 387, 394, 400, 404, 414, 422, 422, 427, 430, 435, 457, 493, 506, 527, 531, 538, 541, 546, 568, 583, 585, 587, 650, 652, 677, 691, 730, 737, 740, 751, 755, 764, 778, 783, 785, 789, 794, 803, 809, 815, 847, 858, 863, 863, 874, 887, 896, 916, 920, 926, 927, 930, 933, 957, 981, 997},
+				target:  542,
+			},
+			want: []int{24, 32},
+		},
+		{
+			name: "6",
+			args: args{
+				numbers: []int{0, 0, 3, 4},
+				target:  0,
+			},
+			want: []int{1, 2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := twoSum(tt.args.numbers, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("twoSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := twoSumBiSe(tt.args.numbers, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("twoSumBiSe() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
