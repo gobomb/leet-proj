b997de6d23b2e4625aaf8d95d1f8307164be4e7d
gobomb <bombcoder@outlook.com>
2021-11-09 11:25:37 +0000

    add leetcode 140. Word Break II

--- pkg/leetcode/medium/139.go
+++ pkg/leetcode/medium/139.go
@@ -34,3 +34,42 @@ func checkWordInDict(s string, wordDict []string) bool {
 
 	return false
 }
+
+/*
+	140. Word Break II
+*/
+
+// https://leetcode.com/problems/word-break-ii/discuss/440571/golang-solution
+func wordBreakII(s string, wordDict []string) []string {
+	if s == "" || len(wordDict) == 0 {
+		return []string{}
+	}
+
+	return dfsWordBreak(s, wordDict, 0, make(map[int][]string))
+}
+
+func dfsWordBreak(s string, wordDict []string, index int, record map[int][]string) []string {
+	if result, ok := record[index]; ok {
+		return result
+	}
+
+	result := []string{}
+
+	for i := index + 1; i <= len(s); i++ {
+		if checkWordInDict(s[index:i], wordDict) {
+			if i != len(s) {
+				rs := dfsWordBreak(s, wordDict, i, record)
+				for _, r := range rs {
+					result = append(result, s[index:i]+" "+r)
+				}
+			} else {
+				result = append(result, s[index:i])
+				break
+			}
+		}
+	}
+
+	record[index] = result
+
+	return result
+}
--- pkg/leetcode/medium/139_test.go
+++ pkg/leetcode/medium/139_test.go
@@ -1,6 +1,10 @@
 package medium
 
-import "testing"
+import (
+	"reflect"
+	"sort"
+	"testing"
+)
 
 func Test_wordBreak(t *testing.T) {
 	type args struct {
@@ -43,6 +47,56 @@ func Test_wordBreak(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := wordBreak(tt.args.s, tt.args.wordDict); got != tt.want {
 				t.Errorf("wordBreak() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_wordBreakII(t *testing.T) {
+	type args struct {
+		s        string
+		wordDict []string
+	}
+	
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "1",
+			args: args{
+				s:        "catsanddog",
+				wordDict: []string{"cat", "cats", "and", "sand", "dog"},
+			},
+			want: []string{"cats and dog", "cat sand dog"},
+		},
+		{
+			name: "2",
+			args: args{
+				s:        "pineapplepenapple",
+				wordDict: []string{"apple", "pen", "applepen", "pine", "pineapple"},
+			},
+			want: []string{"pine apple pen apple", "pineapple pen apple", "pine applepen apple"},
+		},
+		{
+			name: "3",
+			args: args{
+				s:        "catsandog",
+				wordDict: []string{"cats", "dog", "sand", "and", "cat"},
+			},
+			want: []string{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := wordBreakII(tt.args.s, tt.args.wordDict)
+			sort.Strings(got)
+			sort.Strings(tt.want)
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("wordBreakII() = %#v, want %#v", got, tt.want)
 			}
 		})
 	}
