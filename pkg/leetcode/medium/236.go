c611d7c57d90af50920f49292361dddea1b6f5ae
gobomb <bombcoder@outlook.com>
2021-12-05 07:52:38 +0000

    add leetcode 236

---
+++ pkg/leetcode/medium/236.go
@@ -0,0 +1,32 @@
+package medium
+
+/*
+	236. Lowest Common Ancestor of a Binary Tree
+*/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil || p.Val == root.Val || q.Val == root.Val {
+		return root
+	}
+
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+
+	if left != nil {
+		if right != nil {
+			return root
+		} else {
+			return left
+		}
+	}
+
+	return right
+}
---
+++ pkg/leetcode/medium/236_test.go
@@ -0,0 +1,53 @@
+package medium
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_lowestCommonAncestor(t *testing.T) {
+	type args struct {
+		root *TreeNode
+		p    *TreeNode
+		q    *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		{
+			name: "",
+			args: args{
+				root: MakeTree(3, 5, 1, 6, 2, 0, 8, null, null, 7, 4),
+				p:    MakeTree(5),
+				q:    MakeTree(1),
+			},
+			want: MakeTree(3, 5, 1, 6, 2, 0, 8, null, null, 7, 4),
+		},
+		{
+			name: "",
+			args: args{
+				root: MakeTree(3, 5, 1, 6, 2, 0, 8, null, null, 7, 4),
+				p:    MakeTree(5),
+				q:    MakeTree(4),
+			},
+			want: MakeTree(5, 6, 2, null, null, 7, 4),
+		},
+		{
+			name: "",
+			args: args{
+				root: MakeTree(2, 1),
+				p:    MakeTree(2),
+				q:    MakeTree(1),
+			},
+			want: MakeTree(2, 1),
+		}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lowestCommonAncestor(tt.args.root, tt.args.p, tt.args.q); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("lowestCommonAncestor() = %+v, want %+v", got, tt.want)
+			}
+		})
+	}
+}
