13af02d4af7f048a837c34fc02e910153805f1f4
gobomb <bombcoder@outlook.com>
2021-12-08 06:55:26 +0000

    add leetcode 238

---
+++ pkg/leetcode/medium/238.go
@@ -0,0 +1,25 @@
+package medium
+
+import "log"
+
+/*
+	238. Product of Array Except Self
+*/
+
+func productExceptSelf(nums []int) []int {
+	res := make([]int, len(nums))
+	res[0] = 1
+
+	for i := 1; i < len(nums); i++ {
+		res[i] = res[i-1] * nums[i-1]
+	}
+
+	rightProduct := nums[len(nums)-1]
+
+	for i := len(nums) - 2; i >= 0; i-- {
+		res[i] *= rightProduct
+		rightProduct = rightProduct * nums[i]
+	}
+
+	return res
+}
