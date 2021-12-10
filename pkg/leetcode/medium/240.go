98421aad0d551b8da027ca7886a988870fea3f69
gobomb <bombcoder@outlook.com>
2021-12-10 08:40:41 +0000

    add leetcode 240

---
+++ pkg/leetcode/medium/240.go
@@ -0,0 +1,30 @@
+package medium
+
+import (
+	"sort"
+)
+
+/*
+	240. Search a 2D Matrix II
+*/
+
+func searchMatrix(matrix [][]int, target int) bool {
+	cal, row := 0, 0
+	row = len(matrix)
+	
+	if len(matrix) != 0 {
+		cal = len(matrix[0])
+	}
+
+	for i := 0; i < row; i++ {
+		got := sort.Search(cal, func(j int) bool {
+			return matrix[i][j] >= target
+		})
+
+		if got < cal && matrix[i][got] == target {
+			return true
+		}
+	}
+
+	return false
+}
