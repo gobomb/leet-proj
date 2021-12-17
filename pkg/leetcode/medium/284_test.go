40f03500f10fba78f390847e64e5b69e3bf5ef6d
gobomb <bombcoder@outlook.com>
2021-12-17 07:55:33 +0000

    update 284_test: use example test

--- pkg/leetcode/medium/284_test.go
+++ pkg/leetcode/medium/284_test.go
@@ -1,31 +1,24 @@
 package medium
 
 import (
-	"log"
-	"testing"
+	"fmt"
 )
 
-func Test_peekingConstructor(t *testing.T) {
+func ExamplePeekingIterator() {
 	peekingIterator := peekingConstructor(&Iterator{
 		sl: []int{1, 2, 3},
 	}) // [1,2,3]
-	var rs int
-	rs = peekingIterator.peek() // return 2, the pointer does not move [1,2,3].
-	log.Println(rs)
 
-	rs = peekingIterator.next() // return 1, the pointer moves to the next element [1,2,3].
-	log.Println(rs)
+	rs := peekingIterator.peek()
+	fmt.Println(rs)
 
-	rs = peekingIterator.peek() // return 2, the pointer does not move [1,2,3].
-	log.Println(rs)
-
-	rs = peekingIterator.next() // return 2, the pointer moves to the next element [1,2,3]
-	log.Println(rs)
-
-	rs = peekingIterator.next() // return 3, the pointer moves to the next element [1,2,3]
-	log.Println(rs)
-
-	// return False
-	log.Println(peekingIterator.hasNext())
-
+	fmt.Println(peekingIterator.next())
+	fmt.Println(peekingIterator.peek())
+	fmt.Println(peekingIterator.next())
+	fmt.Println(peekingIterator.next())
+	// OutPut: 1
+	// 1
+	// 2
+	// 2
+	// 3
 }
